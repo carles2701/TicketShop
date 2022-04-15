000334775f9e5dc178d7546b74a27e5124fd429b
carles2701 <carles2701@gmail.com>
2022-04-15 21:52:14 +0200

    15.04.2022

--- src/main/java/com/carles2701/TicketShop/controller/ShoppingCartController.java
+++ src/main/java/com/carles2701/TicketShop/controller/ShoppingCartController.java
@@ -1,9 +1,11 @@
 package com.carles2701.TicketShop.controller;
 
 import com.carles2701.TicketShop.globalCart.ShoppingCart;
+import com.carles2701.TicketShop.model.Ticket;
 import com.carles2701.TicketShop.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,4 +20,23 @@ public class ShoppingCartController {
         return "redirect:/shop";
     }
 
+    @GetMapping("/cart")
+    public String getShopCart(Model model){
+        model.addAttribute("size_shopping_cart", ShoppingCart.shoppingCart.size());
+        model.addAttribute("all_elements_of_cart", ShoppingCart.shoppingCart.stream().mapToDouble(Ticket::getPrice).sum());
+        model.addAttribute("cart", ShoppingCart.shoppingCart);
+        return "shoppingCartPage";
+    }
+
+    @GetMapping("/cart/removeItem/{index}")
+    public String ShopCartItemRemove(@PathVariable int index){
+        ShoppingCart.shoppingCart.remove(index);
+        return "redirect:/cart";
+    }
+
+    @GetMapping("/summary")
+    public String summary(Model model){
+        model.addAttribute("all_elements_of_cart", ShoppingCart.shoppingCart.stream().mapToDouble(Ticket::getPrice).sum());
+        return "summaryPage";
+    }
 }
